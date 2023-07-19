cd18c885a817d60fafb76412654d81e56a238083
Atchin Parfait <55301545+kodak10@users.noreply.github.com>
2023-07-19 21:47:57 +0200

    Initialisation du Front end

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,24 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('index');
+});
+Route::get('/about', function () {
+    return view('about');
+});
+Route::get('/blog', function () {
+    return view('blog');
+});
+Route::get('/contact', function () {
+    return view('contact');
+});
+
+Route::get('/login', function () {
+    return view('login');
+});
+Route::get('/register', function () {
+    return view('register');
+});
+Route::get('/services', function () {
+    return view('services');
 });
