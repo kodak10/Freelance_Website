a3515873483c3b70f1d2cccb5a7d8cd71dce4dbf
Atchin Parfait <55301545+kodak10@users.noreply.github.com>
2023-07-21 16:11:04 +0200

    Finalisation Dashbaord Admin and login et register page

---
+++ resources/views/Administration/layouts/sidebar-mobile.blade.php
@@ -0,0 +1,17 @@
+<div class="dashboard_navigationbar d-block d-lg-none">
+    <div class="dropdown">
+        <button onclick="myFunction()" class="dropbtn"><i class="fa fa-bars pr10"></i>  Navigation</button>
+        <ul id="myDropdown" class="dropdown-content">
+            <li class="active"><a href="/administration"><i class="flaticon-home mr10"></i>Menu Général</a></li>
+            <li><p class="fz15 fw400 ff-heading mt30 pl30">Gestion des inscriptions</p></li>
+            <li><a href="#"><i class="flaticon-document mr10"></i>Demandes d'inscription</a></li>
+            <li><a href="#"><i class="flaticon-like mr10"></i>Demandes de Services</a></li>
+            <li><p class="fz15 fw400 ff-heading mt30 pl30">Paramétrage</p></li>
+            <li><a href="#"><i class="flaticon-chat mr10"></i>Les Départements</a></li>
+            <li><a href="#"><i class="flaticon-review-1 mr10"></i>Les Services</a></li>
+            <li><p class="fz15 fw400 ff-heading mt30 pl30">Comptes</p></li>
+            <li><a href="#"><i class="flaticon-receipt mr10"></i>Accès utilisateurs</a></li>
+            <li><a href="#"><i class="flaticon-logout mr10"></i>Se Déconnecter</a></li>
+        </ul>
+    </div>
+</div>
---
+++ resources/views/Administration/layouts/sidebar.blade.php
@@ -0,0 +1,29 @@
+<div class="dashboard__sidebar d-none d-lg-block">
+    <div class="dashboard_sidebar_list">
+        <div class="sidebar_list_item">
+            <a href="#" class="items-center -is-active"><i class="flaticon-home mr15"></i>Menu Général</a>
+        </div>
+        <p class="fz15 fw400 ff-heading pl30 mt30">Gestion des inscriptions</p>
+        <div class="sidebar_list_item">
+            <a href="#" class="items-center"><i class="flaticon-document mr15"></i>Demandes d'inscription</a>
+        </div>
+        <div class="sidebar_list_item">
+            <a href="#" class="items-center"><i class="flaticon-like mr15"></i>Demandes de Services </a>
+        </div>
+        <p class="fz15 fw400 ff-heading pl30 mt30">Paramétrage</p>
+        <div class="sidebar_list_item ">
+            <a href="#" class="items-center"><i class="flaticon-presentation mr15"></i>Les Départements</a>
+        </div>
+        <div class="sidebar_list_item ">
+            <a href="#" class="items-center"><i class="flaticon-briefcase mr15"></i>Les Services</a>
+        </div>
+
+        <p class="fz15 fw400 ff-heading pl30 mt30">Comptes</p>
+        <div class="sidebar_list_item ">
+            <a href="#" class="items-center"><i class="flaticon-user mr15"></i>Accès utilisateurs</a>
+        </div>
+        <div class="sidebar_list_item ">
+            <a href="#" class="items-center"><i class="flaticon-logout mr15"></i>Se Déconnecter</a>
+        </div>
+    </div>
+</div>
