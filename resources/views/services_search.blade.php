48e48315af37e48360828930b563df4407f00cac
Atchin Parfait <55301545+kodak10@users.noreply.github.com>
2023-08-27 13:40:09 +0000

    push

--- resources/views/services_search.blade.php
+++ resources/views/services_search.blade.php
@@ -25,27 +25,26 @@
 </style>
 @section('content')
     <div class="container">
-        
 
         <h4 class="">Résultats de la recherche</h4>
 
         <div class="row mt-5">
-            @foreach ($services as $service)
-            <div class="col-lg-3">
-                <div class="listing-style1">
-                    <div class="list-thumb">
-                        <img class="w-100" src="{{asset('assets/images/listings/g-1.jpg')}}" alt="Image">
-                        <a href="#" class="listing-fav fz12"><span class="far fa-heart"></span></a>
+            @forelse ($services as $service)
+                <div class="col-lg-3">
+                    <div class="listing-style1">
+                        <div class="list-thumb">
+                            <img class="w-100" src="{{asset('assets/images/listings/g-1.jpg')}}" alt="Image">
+                            <a href="#" class="listing-fav fz12"><span class="far fa-heart"></span></a>
+                        </div>
+                        <div class="list-content">
+                            <li></li>
+                            <p class="list-text body-color fz14 mb-1"><a href="{{ route('EntrepriseService.show', ['slug' => $service->id]) }}">{{$service->libelle}}</a></p>
+                        </div>
                     </div>
-                    <div class="list-content">
-                        <li></li>
-                        <p class="list-text body-color fz14 mb-1"><a href="{{ route('EntrepriseService.show', ['slug' => $service->id]) }}">{{$service->libelle}}</a></p>
-
-                    </div>
-
                 </div>
-            </div>
-        @endforeach
+                @empty
+                    <span>Aucun résultat pour votre recherche</span>
+            @endforelse
         </div>
 
     </div>
