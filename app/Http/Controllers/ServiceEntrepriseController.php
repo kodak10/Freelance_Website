f35d967e0fb4970e92952211a3847fd73f8d9bcd
Kodak10 <atchinaymard10@gmail.com>
2023-10-09 20:11:24 +0000

    push

--- app/Http/Controllers/ServiceEntrepriseController.php
+++ app/Http/Controllers/ServiceEntrepriseController.php
@@ -19,7 +19,7 @@ class ServiceEntrepriseController extends Controller
         $departements = Departement::get();
         $service = Service::get();
         $Counter = 1;
-        return view('Entreprise.service.index', compact('services', 'departements', 'Counter', 'service'));
+        return view('Entreprise.Service.index', compact('services', 'departements', 'Counter', 'service'));
 
     }
 
