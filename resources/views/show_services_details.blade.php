9233f22b16617fa13b03f8fb33526bd063a6daa5
unknown <atchinaymard10@gmail.com>
2024-01-03 09:47:42 +0100

    fusion front v2 and save all dashboard

--- resources/views/show_services_details.blade.php
+++ resources/views/show_services_details.blade.php
@@ -163,9 +163,9 @@
             <div class="video-thumb">
 
                 <ul class="thumb-menu menu-tab2">
-                  <li class="ct-tab2"> <a class="lightbox-gallery" href="{{asset('assets/website/images/review/thumbv4.jpg')}}"><img src="{{asset('assets/images/review/thumbv4.jpg')}}" alt="images"></a> </li>
-                  <li class="ct-tab2"> <a class="lightbox-gallery" href="{{asset('assets/website/images/review/thumbv1.jpg')}}"><img src="{{asset('assets/images/review/thumbv1.jpg')}}" alt="images"></a></li>
-                  <li class="ct-tab2"><a class="lightbox-gallery" href="{{asset('assets/website/images/review/thumbv2.jpg')}}"><img src="{{asset('assets/images/review/thumbv2.jpg')}}" alt="images"></a></li>
+                  <li class="ct-tab2"> <a class="lightbox-gallery" href="{{asset('assets/website/images/review/thumbv4.jpg')}}"><img src="{{asset('assets/website/images/review/thumbv4.jpg')}}" alt="images"></a> </li>
+                  <li class="ct-tab2"> <a class="lightbox-gallery" href="{{asset('assets/website/images/review/thumbv1.jpg')}}"><img src="{{asset('assets/website/images/review/thumbv1.jpg')}}" alt="images"></a></li>
+                  <li class="ct-tab2"><a class="lightbox-gallery" href="{{asset('assets/website/images/review/thumbv2.jpg')}}"><img src="{{asset('assets/website/images/review/thumbv2.jpg')}}" alt="images"></a></li>
                 </ul>
               </div>
         </div>
